57042125be08baa8e68878900126fdc201327b7e
kaur5831 <50632645+kaur5831@users.noreply.github.com>
2020-08-04 23:02:10 -0400

    Add files via upload

---
+++ assets/data/mydata.ts
@@ -0,0 +1,11 @@
+import { kaur5831 } from './../../app/kaur5831';
+
+
+export const MYINFO : kaur5831 = {
+    stdID: '991532026',
+    stdName:'Amandeep Kaur',
+    stdLoginName:'kaur5831',
+    stdCampus:'Davis',
+    stdAssignTitle:'Assignment 4'
+
+}
